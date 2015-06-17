de144ef3d122abdac2f410c8b6b724433fb7f101
kenvi <mrwhite@163.com>
2015-06-17 23:46:18 +0800

    add dispatcher thread

---
+++ src/com/kenvifire/jmemcached/DispatcherThread.java
@@ -0,0 +1,14 @@
+package com.kenvifire.jmemcached;
+
+/**
+ * Created by hannahzhang on 15/6/17.
+ */
+public class DispatcherThread implements Runnable{
+    private Thread[] workerThreads;
+
+    @Override
+    public void run() {
+
+
+    }
+}
